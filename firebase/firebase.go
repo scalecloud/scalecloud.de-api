bf5cdaaee9176577d9fdb162e9d81bc9c663daf3
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-04-23 11:33:20 +0200

    Added firebase module

---
+++ firebase/firebase.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go/v4"
+)
+
+func main() {
+}
+
+func initializeAppDefault() *firebase.App {
+	// [START initialize_app_default_golang]
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+	// [END initialize_app_default_golang]
+
+	return app
+}
