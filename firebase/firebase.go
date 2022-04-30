eafd1d3d4e1f3a94a213539dfc43997c91d119e0
Sembeth <46921730+Sembeth@users.noreply.github.com>
2022-04-30 21:18:41 +0200

    Improved and testet token verification

--- firebase/firebase.go
+++ firebase/firebase.go
@@ -2,7 +2,7 @@ package firebase
 
 import (
 	"context"
-	"log"
+	"os"
 
 	"go.uber.org/zap"
 
@@ -10,40 +10,71 @@ import (
 	"google.golang.org/api/option"
 )
 
+const keyFile = "./keys/firebase-serviceAccountKey.json"
+
 var logger, _ = zap.NewProduction()
 
 func InitFirebase() {
 	logger.Info("Init firebase")
-	verifyIDToken(context.Background(), InitializeAppDefault(), "potato")
+	if fileExists(keyFile) {
+		logger.Info("Keyfile exists. ", zap.String("file", keyFile))
+	} else {
+		logger.Error("Keyfile does not exist. ", zap.String("file", keyFile))
+		os.Exit(1)
+	}
+
+	token := ""
+	uid := ""
+	ctx := context.Background()
+	app := InitializeAppDefault(ctx)
+	if verifyIDToken(ctx, app, token, uid) {
+		logger.Info("Token is valid.")
+	} else {
+		logger.Info("Token is not valid.")
+	}
+
 }
 
-func InitializeAppDefault() *firebase.App {
-
-	opt := option.WithCredentialsFile("./keys/firebase-serviceAccountKey.json")
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		logger.Error("Error initializing app", zap.Error(err))
-		return nil
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
 	}
-	logger.Info("Firebase app initialized successfully")
+	return !info.IsDir()
+}
 
+func InitializeAppDefault(ctx context.Context) *firebase.App {
+
+	opt := option.WithCredentialsFile(keyFile)
+
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		logger.Error("Error creating new Firebase app.", zap.Error(err))
+	}
 	return app
 }
 
-func verifyIDToken(ctx context.Context, app *firebase.App, idToken string) bool {
-	// [START verify_id_token_golang]
-	client, err := app.Auth(ctx)
-	if err != nil {
-		log.Fatalf("error getting Auth client: %v\n", err)
+func verifyIDToken(ctx context.Context, app *firebase.App, awtToken string, uid string) bool {
+	ret := false
+	if awtToken == "" {
+		logger.Error("ID token is empty")
+	} else if uid == "" {
+		logger.Error("UID is empty")
+	} else {
+		client, err := app.Auth(ctx)
+		if err != nil {
+			logger.Error("Error initializing app", zap.Error(err))
+		} else {
+			token, err := client.VerifyIDTokenAndCheckRevoked(ctx, awtToken)
+			if err != nil {
+				logger.Error("Error verifying ID token", zap.Error(err))
+			} else if token.UID != uid {
+				logger.Error("UID does not match with Token", zap.Any("Token:", token))
+			} else if token.UID == uid {
+				logger.Info("Token is valid and matches UID.", zap.Any("Token:", token))
+				ret = true
+			}
+		}
 	}
-
-	token, err := client.VerifyIDToken(ctx, idToken)
-	if err != nil {
-		log.Fatalf("error verifying ID token: %v\n", err)
-	}
-
-	log.Printf("Verified ID token: %v\n", token)
-	// [END verify_id_token_golang]
-
-	return true
+	return ret
 }
