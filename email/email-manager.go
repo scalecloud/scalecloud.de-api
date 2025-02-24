5ddc9380f729f99bca1f4fd303bd2c8e63308339
sembeth <sembeth@live.de>
2025-02-24 22:31:56 +0100

    Implemented manager for E-Mail sending.

--- email/email-manager.go
+++ email/email-manager.go
@@ -1,10 +1,43 @@
 package email
 
 import (
-	"context"
-	"errors"
+	"go.uber.org/zap"
+	"gopkg.in/gomail.v2"
 )
 
-func /* (emailHandler *EMailHandler) */ sendEMail(c context.Context) error {
-	return errors.New("not implemented yet")
+type SMTPConnection struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	From     string
+}
+
+type EMailHandler struct {
+	EMailConnection *SMTPConnection
+	Log             *zap.Logger
+}
+
+type Email struct {
+	To      []string
+	Subject string
+	Body    string
+}
+
+func (emailHandler *EMailHandler) sendEMail(email Email) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", emailHandler.EMailConnection.From)
+	m.SetHeader("To", email.To...)
+	m.SetHeader("Subject", email.Subject)
+	m.SetBody("text/html", email.Body)
+
+	d := gomail.NewDialer(emailHandler.EMailConnection.Host, emailHandler.EMailConnection.Port, emailHandler.EMailConnection.Username, emailHandler.EMailConnection.Password)
+
+	if err := d.DialAndSend(m); err != nil {
+		emailHandler.Log.Error("Failed to send email", zap.Error(err))
+		return err
+	}
+
+	emailHandler.Log.Info("Email sent successfully")
+	return nil
 }
--- go.mod
+++ go.mod
@@ -96,6 +96,8 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250219182151-9fdb1cabc7b2 // indirect
 	google.golang.org/grpc v1.70.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
